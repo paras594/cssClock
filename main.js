5ae3b04597e8fa2b10f33bdfe8c12ff047ed3ae0
paras594 <parasarora594@gmail.com>
2018-09-02 13:10:41 +0530

    completed the project

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-alert("working");
+const secondHand = document.querySelector(".second-hand");
+const minHand = document.querySelector(".min-hand");
+const hoursHand = document.querySelector(".hour-hand");
+function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+    const secondsDegree = (seconds / 60) * 360 + 90;
+    const minutesDegree = (minutes / 60) * 360 + 90;
+    const hoursDegree = (hours / 12) * 360 + 90;
+
+    secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+    minHand.style.transform = `rotate(${minutesDegree}deg)`;
+    hoursHand.style.transform = `rotate(${hoursDegree}deg)`;
+
+    console.log(minutes + " " + seconds + " " + hours);
+}
+
+setInterval(setDate, 1000);
